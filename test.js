21576052446dca8cc6014815b955b6d116acad78
dialixx <105409038+dialixx@users.noreply.github.com>
2022-06-22 11:44:04 -0500

    display inline

--- test.js
+++ test.js
@@ -59,7 +59,7 @@ function statroll() {
 
 function statroll20() {
 	document.getElementById("output2").innerHTML += "> lol just roll 6 d20 or make them up<br>";
-	document.getElementById("d20").style = "";
+	document.getElementById("d20").style = "display: inline;";
 }
 
 function statroll20real() {
